455634dbacc1bdfd065dcf02cc8489b4df44cff3
Malte Bublitz <malte@rolltreppe3.de>
2025-08-31 14:12:58 +0200

    Improved run.sh
    - Using git with --quiet option
    - Default action is now build
    - Added test action for debugging
      (Uses links to render status.html to plain text, and sends a push
      notification using Pushover)

--- run.sh
+++ run.sh
@@ -8,7 +8,40 @@ SSH_KEY="$HOME/.ssh/id_tinystatus"
 
 do_build() {
 	echo " [run.sh]  Generate status page"
-	./tinystatus > status.html
+	./tinystatus \
+		2>/dev/null \
+		> status.html
+}
+
+do_test() {
+	# echo ">>> FEATURE DISABLED <<<" >&2
+	# return 9
+	
+	# Check for requirements
+	if ! command -v links 2>&1 >/dev/null; then
+		echo " [run.sh]  [test]  Error: Command not found: links"
+		return 1
+	elif ! command -v po-notify.pyz 2>&1 >/dev/null; then
+		echo " [run.sh]  [test]  Error: Command not found: po-notify.pyz"
+		return 1
+	fi
+	
+	# do_build || return 2
+	
+	# echo " [run.sh]  [test]  Create a backup of status.html"
+	# cp status.html status.$(date +%Y-%m-%d_%H-%M).html
+
+	echo " [run.sh]  [test]  Send status.html using Pushover"
+	(
+		echo -e "A plain text version of status.html:\n\n"
+		links -dump status.html
+	) | \
+		sed 's/^[ \t]*//;s/[ \t]*$//g' | \
+		env \
+			PUSHOVER_PRIORITY=-1 \
+			po-notify.pyz \
+			"Update for status.rolltreppe3.de"
+
 }
 
 do_upload() {
@@ -28,20 +61,24 @@ do_upload() {
 	git pull
 	
 	echo " [run.sh]  Copy status.html to gh-pages"
-	#cp status.html gh-pages/index.html
 	cp ../status.html index.html
 	
 	echo " [run.sh]  Git: Commit changes to index.html in gh-pages"
 	git add index.html
 	if git diff --cached --exit-code >/dev/null; then
 		# Only run git commit if changes to index.html where made
-		git commit -m "status page updated."
+		git commit --quiet -m "status page updated."
+	# else
+	# 	# DEBUG
+	# 	echo "git diff --cached --exit-code \!= 0" \
+	# 		~/bin/po-notify.pyz "status :: run.sh"
+	# 	git commit --quiet -m "status page updated."
 	fi
 	
 	echo " [run.sh]  Git: Push gh-pages branch"
 	env \
 		GIT_SSH_COMMAND="ssh -i ${SSH_KEY}" \
-		git push
+		git push --quiet
 	
 	popd >/dev/null
 }
@@ -50,21 +87,23 @@ do_upload() {
 if [[ "$1" == "--help" || "$1" == "-h" || "$1" == "help" ]]; then
 	echo "status.rolltreppe3.de :: run.sh"
 	echo "Usage:"
-	echo -e "\t./run.sh [build|upload|help]"
+	echo -e "\t./run.sh [build|upload|test|help]"
 	exit 0
 fi
 
-if [[ "$#" -eq 0 ]]; then
-	echo "Startibg default action ..."
-	do_upload
-fi
-
 case "$1" in
-	build)
+	""|"build")
 		do_build
 		;;
-	upload)
+	"test")
+		do_build
+		do_test
+		;;
+	"upload")
+		#[ -f ~/tinystatus.lock ] 
+		printf "PID=$$\nPWD=$PWD\nargv0=$0\nARGS=$@\n" > ~/tinystatus.lock
 		do_upload
+		rm -f ~/tinystatus.lock
 		;;
 	*)
 		echo "Error: Unknown action \"$1\"" >&2
